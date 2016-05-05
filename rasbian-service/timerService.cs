2886dc81bdd3d5a029925ffbf57f7d1189da16ff
Josh Rolstad <jrolstad@gmail.com>
2016-05-04 21:46:17 -0700

    Initial commit for service

---
+++ rasbian-service/timerService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Media;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace rasbian_service
+{
+    public partial class TimerService : ServiceBase
+    {
+        private readonly Timer _timer;
+
+        public TimerService()
+        {
+            InitializeComponent();
+
+            _timer = new Timer {Interval = TimeSpan.FromSeconds(10).TotalMilliseconds};
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var filePath = "my-file.txt";
+            var message = string.Format("{0} - {1}",DateTime.Now,Environment.OSVersion);
+            System.IO.File.AppendAllText(filePath,message);
+
+            SystemSounds.Beep.Play();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            _timer.Stop();
+        }
+    }
+}
